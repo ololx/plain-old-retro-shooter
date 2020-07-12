d99c727fd69185842b001490ea69978869b7f372
Alexander A. Kropotin <ololx@icloud.com>
2020-07-12 11:17:58 +0500

    chore(/org/plain/old/retro/shooter/monitor/Screen.java) add rays for camera

--- src/org/plain/old/retro/shooter/monitor/Screen.java
+++ src/org/plain/old/retro/shooter/monitor/Screen.java
@@ -3,6 +3,10 @@ package org.plain.old.retro.shooter.monitor;
 import org.plain.old.retro.shooter.math.Vector2d;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 public class Screen {
 
@@ -25,23 +29,45 @@ public class Screen {
         int xx = (int) ((pos.getX() * this.cellSize));
         int yy = (int) ((pos.getY() * this.cellSize));
 
-        int xx2 = (int) ((dir.getX() / 4 * this.cellSize));
-        int yy2 = (int) ((dir.getY() / 4 * this.cellSize));
         int offset = this.cellSize / 10;
 
-        Vector2d ray = pos.clone();
+        Vector2d sideRay = dir.rotate(30);
+        double angleBetween = dir.getAngle(sideRay);
+        List<Vector2d> rays = new ArrayList<>();
+        for (double angle = -angleBetween; angle <= angleBetween; angle += angleBetween / 5) {
+            Vector2d rayDir = dir.clone().rotate(angle);
+            Vector2d ray = pos.clone();
 
-        boolean hit = false;
-        while (!hit) {
-            Vector2d temp_ray = ray.clone().add(dir.multiply(0.01));
+            boolean hit = false;
+            while (!hit) {
+                Vector2d temp_ray = ray.clone().add(rayDir.multiply(0.1));
 
-            if (map[(int) temp_ray.getX()][(int) temp_ray.getY()] != 0) hit = true;
+                if (map[(int) temp_ray.getX()][(int) temp_ray.getY()] != 0) hit = true;
 
-            ray = temp_ray.clone();
+                ray = temp_ray.clone();
+            }
+
+            rays.add(ray);
         }
 
-        int xx3 = (int) ((ray.getX() * this.cellSize));
-        int yy3 = (int) ((ray.getY() * this.cellSize));
+        /*for(int i = pixels.length/2; i< pixels.length; i++){
+            if(pixels[i] != Color.BLUE.getRGB()) pixels[i] = Color.BLUE.getRGB();
+        }
+
+        for(int i = 0; i< pixels.length/2; i++){
+            if(pixels[i] != Color.GREEN.getRGB()) pixels[i] = Color.GREEN.getRGB();
+        }
+
+        for(int i = 0; i < rays.size(); i++){
+
+            for (int k = i * width; k < width / rays.size(); k++) {
+
+                double hight = rays.get(i).subtract(pos).getModule();
+                for (int j = 0; j < height / hight; j++) {
+                    pixels[i + j * width] = Color.RED.getRGB();
+                }
+            }
+        }*/
 
         for (int i = 0; i < p; i++) {
             for (int j = 0; j < q; j++) {
@@ -55,14 +81,15 @@ public class Screen {
                             pixels[(n * width) + nn] = Color.RED.getRGB();
                         }
 
-                        if (n >= (xx2 + xx) - offset && n <= (xx2 + xx) + offset
-                                && nn >= (yy2 + yy) - offset && nn <= (yy2 + yy) + offset) {
-                            pixels[(n * width) + nn] = Color.RED.getRGB();
-                        }
+                        for (Vector2d ray : rays) {
 
-                        if (n >= xx3 - offset && n <= xx3 + offset
-                                && nn >= yy3 - offset && nn <= yy3 + offset) {
-                            pixels[(n * width) + nn] = Color.CYAN.getRGB();
+                            int xx3 = (int) ((ray.getX() * this.cellSize));
+                            int yy3 = (int) ((ray.getY() * this.cellSize));
+
+                            if (n >= xx3 - offset && n <= xx3 + offset
+                                    && nn >= yy3 - offset && nn <= yy3 + offset) {
+                                pixels[(n * width) + nn] = Color.CYAN.getRGB();
+                            }
                         }
                     }
                 }
